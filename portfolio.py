10ad68ae17c2b0058d0846c16e23e5c29f507971
Dwz03 <dwz20030514@gmail.com>
2026-08-19 18:34:11 +0800

    I create a portfolio class and connect it with the position class

---
+++ portfolio.py
@@ -0,0 +1,117 @@
+# list查找是O(n)复杂度， dict是O(1)复杂度
+
+positions = {}
+
+positions["AAPL"] = 100
+positions["MSFT"] = 50
+
+print(positions)
+print(positions["AAPL"])
+
+print(positions.get("AAPL"))
+print(positions.get("NVDA"))
+
+positions['AAPL'] = 150
+print(positions)
+
+print("AAPL" in positions)
+print("NVDA" in positions)
+
+class Position:
+
+    def __init__(self,symbol,quantity):
+        self.symbol = symbol
+        self.quantity = quantity
+
+    def market_value(self,price):
+        return self.quantity * price
+
+    def update_quantity(self,new_quantity):
+        self.quantity = new_quantity
+
+    def is_long(self):
+        if self.quantity > 0:
+            return True
+        else:
+            return False
+
+class Portfolio:
+    def __init__(self, cash):
+        self.cash = cash
+        self.positions = {}
+
+    def add_position(self, position):
+        self.positions[position.symbol] = position #我们用symbol当作key，然后进来的parameter position其实是一个object
+
+    def get_position(self, symbol):
+        return self.positions.get(symbol)
+
+    def remove_position(self, symbol):
+        self.positions.pop(symbol, None)
+
+    def show_position(self):
+        for symbol, position in self.positions.items():
+            print(symbol, position.quantity)
+
+    def total_market_value(self, prices):
+        total = 0
+        for symbol, position in self.positions.items():
+            price = prices[symbol]
+            value = position.market_value(price)
+            total = total + value
+
+            print(symbol, value)
+
+        return total
+
+
+portfolio = Portfolio(100000)
+print(portfolio.cash)
+print(portfolio.positions)
+
+aapl = Position("AAPL", 100)
+msft = Position("MSFT", 50)
+
+portfolio.add_position(aapl)
+portfolio.add_position(msft)
+
+print(portfolio.positions["AAPL"].quantity)
+
+aapl_position = portfolio.get_position("AAPL")
+msft_position = portfolio.get_position("MSFT")
+
+print(aapl_position.quantity)
+print(msft_position.quantity)
+
+portfolio.remove_position("MSFT")
+print(portfolio.positions)
+
+aapl.update_quantity(200)
+
+print(aapl.quantity)
+print(portfolio.get_position("AAPL").quantity)
+
+print(portfolio.get_position("NVDA"))
+print(portfolio.get_position("AAPL"))
+
+portfolio.remove_position("NVDA")
+print("Program still running")
+
+portfolio.add_position(msft)
+portfolio.show_position()
+
+print(portfolio.positions)
+
+prices = {
+    "AAPL": 230,
+    "MSFT": 500
+}
+
+print(portfolio.total_market_value(prices))
+
+
+
+
+
+
+
